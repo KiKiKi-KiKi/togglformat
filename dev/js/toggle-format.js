67c85a76a2e43cf700af1f436a53bafd67bf423f
Chaika <chaika.design@gmail.com>
2014-06-07 05:31:42 +0900

    タスクをフォーマットして表示

--- dev/js/toggle-format.js
+++ dev/js/toggle-format.js
@@ -1,23 +1,57 @@
 jQuery.noConflict();
 !function(w, d, $) {
 $(function() {
-  var area = '#time-entry-list',
-      day  = '.time-entries-day',
-      header = '.timer-entries-group-header';
-
   (function() {
-    var $modal = $('<div id="chaika-toggle-format-modal"></div>'),
-        $textarea = $('<textarea></textarea>'),
+    var $main = $('#time-entry-list');
+    if(!$main.length) return;
+    var modalName = 'chaikaToggleFormatModal',
+        $area = $('<div style="display:none;position:fixed;top: 0;left:0;width:100%;height:100%;z-index:1000;"></div>'),
+        $cover = $('<div style="position:absolute;background:rgba(0,0,0,.5);width:100%;height:100%;"></div>'),
+        $modal = $('<div style="position:relative;display:table;width:100%;height:100%;"></div>')
+          .html('<div class="' + modalName + '" style="display:table-cell;padding:0 25px;vertical-align: middle;"><a href="#" class="modalCloseBtn" style="display:inline-block;position:absolute;right:50px;color:#FFF;font-weight:bold;background:#E41A1A;margin-top:10px;padding:5px 10px;">\u00d7 Close</a></div>'),
+        $textarea = $('<textarea style="padding:20px;min-height:70%;"></textarea>'),
         val = '';
-    $modal.append($textarea).appendTo('body');
-    $(area).find(day).each(function(i, elm) {
+    $area.append($cover).append($modal).on('click', '.modalCloseBtn', function(){
+      $area.remove();
+      return false;
+    }).appendTo('body');
+    $('#time-entry-list').find('.time-entries-day').each(function(i, elm) {
       var $e = $(elm),
-          $header = $e.find(header),
+          $header = $e.find('.timer-entries-group-header'),
+          $list = $e.find('.time-entries-list'),
           date = $header.find('.checkbox-container div.button').data('date'),
-          total = $header.find('.date-container .super').text();
+          total = $header.find('.date-container .super').text(),
+          arg = [];
+
       val += "---------------------------\n" + moment(date, "YYYYMMDD").format("MM/DD") + ' : ' + total + "\n";
+      $list.find('.entry').each(function(j, entry) {
+        var $li = $(entry),
+            times = $li.find('.entry-time').text(),
+            description = $li.find('.js-description span').text(),
+            projectName = $li.find('.js-project span.project-project').text(),
+            duration = $li.find('.js-duration .entry-duration').text(),
+            entryVal = '  ', l;
+
+        times = times.split('–');
+        l = times.length;
+        if(l>0) {
+          for(var n=0; n<l; n+=1) {
+            var time = times[n].trim();
+            times[n] = moment(time, "h:mm a").format("HH:mm");
+          }
+          entryVal += times.join(' – ') + '  ';
+        }
+        entryVal += description + ' (' + duration + ')';
+        if(projectName) {
+          entryVal += ' - ' + projectName;
+        }
+        arg[arg.length] = entryVal;
+      });
+      val += arg.reverse().join("\n") + "\n";
     });
-    $textarea.val(val).focus();
+    val += "---------------------------";
+    $area.show().find('.'+modalName).append($textarea.val(val));
+    $textarea.focus();
   })();
 });
 }(window, document, jQuery);
